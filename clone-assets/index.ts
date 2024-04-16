a1731e4dd8a0f422579edb7c11fbe91dc75c897f
Christian Zoppi <cz@storyblok.com>
2024-04-16 02:18:20 +0200

    unused-assets keep a copy feat

--- clone-assets/index.ts
+++ clone-assets/index.ts
@@ -6,10 +6,10 @@ const targetSpaceId = import.meta.env.VITE_TARGET_SPACE_ID;
 const simultaneousUploads = import.meta.env.VITE_SIMULTANEOUS_UPLOADS;
 const sourceSpaceRegion = import.meta.env.VITE_SOURCE_SPACE_REGION;
 const targetSpaceRegion = import.meta.env.VITE_TARGET_SPACE_REGION;
-const clearSource = import.meta.env.VITE_CLEAR_SOURCE === "true";
-const detectImageSize = import.meta.env.VITE_DETECT_IMAGE_SIZE === "true";
-const usedAssetsOnly = import.meta.env.VITE_USED_ASSETS_ONLY === "true";
-const duplicateFolders = import.meta.env.VITE_DUPLICATE_FOLDERS === "true";
+const clearSource = import.meta.env.VITE_CLEAR_SOURCE?.toLowerCase() === "true";
+const detectImageSize = import.meta.env.VITE_DETECT_IMAGE_SIZE?.toLowerCase() === "true";
+const usedAssetsOnly = import.meta.env.VITE_USED_ASSETS_ONLY?.toLowerCase() === "true";
+const duplicateFolders = import.meta.env.VITE_DUPLICATE_FOLDERS?.toLowerCase() === "true";
 
 const migration = new Migration(paToken, sourceSpaceId, targetSpaceId, simultaneousUploads, sourceSpaceRegion, targetSpaceRegion, clearSource, detectImageSize, usedAssetsOnly, duplicateFolders)
 migration.start()
